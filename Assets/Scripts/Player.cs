bfd8da0098b4474ff752939cf9378f679fd11f9f
Karl Thaler <karlthaler5@gmail.com>
2023-07-26 11:36:26 -0700

    Began adding SaveSystem functionality, basic save and load for player position

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -102,7 +102,22 @@ public class Player : MonoBehaviour
 
 
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+    }
 
+    public void SavePlayer()  {
+        SaveSystem.SavePlayer(this);
+    }
+
+    public void LoadPlayer () {
+        PlayerData data = SaveSystem.LoadPlayer();
+
+        Vector3 pos;
+        pos.x = data.position[0];
+        pos.y = data.position[1];
+        pos.z = data.position[2];
+
+        movePoint.position = pos;
+        transform.position = pos;
     }
 
     public int GetOrientation() {
